05c42edaedbc0b53747b3329db6dbb790638769a
sathesh-brandingbeez-dev <sathes.brandingbeez@gmail.com>
2025-09-23 00:39:51 +0530

    Update auth.ts

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const API_BASE_URL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "production"
     ? "/api"
-    : process.env.NEXT_PUBLIC_API_URL || "/api"
+    : process.env.NEXT_PUBLIC_API_BASE || "/api"
 
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
 
